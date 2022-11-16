c77862aefbb856dcb49922cc3878718db30ba343
John <ij69@pm.me>
2022-11-16 15:37:21 +0000

    Update 3 files

--- app.vue
+++ app.vue
@@ -1,15 +1,15 @@
 <script lang="ts" setup>
 const storeMain = useStoreMain();
 const state = reactive({
-  text: '',
+  text: "",
   files: [],
 });
 function postAdd() {
-  console.log('postAdd');
+  console.log("postAdd");
   const postPayload = {
     text: state.text,
   };
-  state.text = '';
+  state.text = "";
   storeMain.postAdd(postPayload);
 }
 </script>
@@ -20,14 +20,28 @@ function postAdd() {
       type="text"
       v-model="state.text"
       placeholder="Enter text"
+      class="bg-red-300"
       @keyup.enter="postAdd()"
     />
-    <div v-for="p in storeMain.posts" :key="p.id">
+    <div
+      v-for="p in storeMain.posts"
+      :key="p.id"
+      class="flex flex-col w-full items-start content-start br"
+    >
       <pre>{{ p }} </pre>
       <button @click="storeMain.postDelete(p.id)">delete post</button>
       Likes: {{ p.likes_count }}
       <button @click="storeMain.postLike(p.id)">Like</button>
       <button @click="storeMain.postDislike(p.id)">Dislike</button>
+      <BaseImg
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNiIAHrL1a5mw8hOHR6gi2ETS7DYWAe6xxl2EdwJ5r&s"
+      />
     </div>
   </div>
 </template>
+
+<style>
+.br {
+  border: 1px solid red;
+}
+</style>
--- components/BaseImg.vue
+++ components/BaseImg.vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+const props = defineProps({
+  src: { type: String },
+});
+</script>
+
 <template>
-  <div class="flex bg-red"></div>
+  <div class="flex bg-blue-300">
+    <img :src="props.src" />
+  </div>
 </template>
